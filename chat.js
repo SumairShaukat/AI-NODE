ef7ad3e436fb7cf0545c72e0cc39bfb4f0d45b84
Muhamad Sumair Shaukat <sumairch224@gmail.com>
2024-04-21 23:55:21 +0500

    added more functions

--- chat.js
+++ chat.js
@@ -5,3 +5,16 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+
+//keep the history and send latest user message to openAI
+const newMessage =  async (history, message) => {
+    const chatCompletion = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        message: [...history, message]
+
+    })
+    return chatCompletion.choices[0].message
+}
+//formats the user message in required structure for openAi
+const formatMessage = (userInput) => ({role: 'user', content: userInput})
